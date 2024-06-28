a0e4a4b087685d10056df424b603db2b777b439c
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-06-28 11:47:14 +0200

    chore: Update Header styling and integrate Card component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "tailwindcss/colors";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -7,6 +10,7 @@ export default {
         foreground: "rgb(var(--foreground))",
         background: "rgb(var(--background))",
         backgroundLifted: "rgb(var(--background-lifted))",
+        secondary: colors.gray[500],
       },
     },
   },
