c83861a80a9dbbfb692918e039232c8a274ea8ae
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-08-21 14:13:08 +0200

    fix navigation on scroll

--- src/utils/contact-form.ts
+++ src/utils/contact-form.ts
@@ -1,8 +1,27 @@
+const TIMESTAMP = "contactFormTimestamp";
+
+const encodeFormData = (data: {}) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 export function sendContactForm(formData: {
   name: string;
   email: string;
   message: string;
 }) {
+  //   const timestamp = localStorage.getItem(TIMESTAMP);
+
+  //   if (timestamp) {
+  //     const diff = Date.now() - parseInt(timestamp, 10);
+  //     if (diff < threeMinutesInMilliseconds) {
+  //       console.error("You can only submit the form once every 5 minutes.");
+  //       return;
+  //     }
+  //   }
+
+  //   localStorage.setItem(TIMESTAMP, Date.now().toString());
   return fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -15,9 +34,3 @@ export function sendContactForm(formData: {
       console.error("Error sending contact form");
     });
 }
-
-const encodeFormData = (data: {}) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-};
--- src/utils/nav-scroll.ts
+++ src/utils/nav-scroll.ts
@@ -3,22 +3,24 @@
     []) as NodeListOf<HTMLElement>;
   const navItems = (document.querySelectorAll(".nav-item") ??
     []) as NodeListOf<HTMLElement>;
-  const offset = 100; // Adjust the offset if necessary
+  const offset = 300;
 
   console.log("sections", sections, navItems);
 
   window.addEventListener("scroll", () => {
-    let currentPosition = window.scrollY + offset;
+    requestAnimationFrame(() => {
+      let currentPosition = window.scrollY + offset;
 
-    sections.forEach((section, index) => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.offsetHeight;
-      const sectionBottom = sectionTop + sectionHeight;
+      sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionBottom = sectionTop + sectionHeight;
 
-      if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
-        navItems.forEach((item) => item.classList.remove("active"));
-        navItems[index].classList.add("active");
-      }
+        if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
+          navItems.forEach((item) => item.classList.remove("active"));
+          navItems[index].classList.add("active");
+        }
+      });
     });
   });
 })();
