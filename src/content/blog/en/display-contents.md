a3153a5909cee41a6607d001dfacd3884c1a034e
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-09-15 20:49:07 +0200

    fix: add favicon, update meta desc, fix mobile menu on init

---
+++ src/content/blog/en/display-contents.md
@@ -0,0 +1,6 @@
+---
+title: "Making Responsive UI Components with display: contents"
+date: 2024-09-15
+platform: Medium
+link: https://medium.com/@johannes.maendle/making-responsive-ui-components-with-display-contents-a694d57685c9
+---
--- src/utils/nav-scroll.ts
+++ src/utils/nav-scroll.ts
@@ -1,11 +1,11 @@
 (function toggleNavClassOnScroll() {
-  const sections = (document.querySelectorAll(".link-section") ??
-    []) as NodeListOf<HTMLElement>;
-  const navItems = (document.querySelectorAll(".nav-item") ??
-    []) as NodeListOf<HTMLElement>;
+  const sections = document.querySelectorAll(
+    ".link-section",
+  ) as NodeListOf<HTMLElement>;
+  const navItems = document.querySelectorAll(
+    ".nav-item",
+  ) as NodeListOf<HTMLElement>;
   const offset = 300;
-
-  console.log("sections", sections, navItems);
 
   window.addEventListener("scroll", () => {
     requestAnimationFrame(() => {
@@ -17,8 +17,13 @@
         const sectionBottom = sectionTop + sectionHeight;
 
         if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
-          navItems.forEach((item) => item.classList.remove("active"));
+          navItems.forEach((item) => {
+            item.classList.remove("active");
+            item.ariaCurrent = "false";
+          });
+
           navItems[index].classList.add("active");
+          navItems[index].ariaCurrent = "page";
         }
       });
     });
